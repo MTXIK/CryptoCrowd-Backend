4b6588a9f1f5b9a59201d8104bb34eb26e339df5
MTXIK <nponomarev7545@gmail.com>
2025-05-24 17:59:45 +0300

    Add users migration

--- migrate/migrations/20250524140326_initial_creation.sql
+++ migrate/migrations/20250524140326_initial_creation.sql
@@ -1,9 +1,25 @@
 -- +goose Up
 -- +goose StatementBegin
-SELECT 'up SQL query';
+CREATE TYPE RoleType AS ENUM ('admin', 'startup', 'investor');
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL
+    role RoleType VARCHAR(50) NOT NULL DEFAULT 'investor',
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX idx_users_username ON users (username);
+CREATE INDEX idx_users_email ON users (email);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-SELECT 'down SQL query';
+DROP TABLE IF EXISTS users;
+DROP TYPE IF EXISTS RoleType;
+DROP INDEX IF EXISTS idx_users_username;
+DROP INDEX IF EXISTS idx_users_email;
 -- +goose StatementEnd
