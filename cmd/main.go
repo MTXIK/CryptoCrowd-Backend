a86c445e9ff9b5552f1545fdc6a50fcfe21f7897
MTXIK <nponomarev7545@gmail.com>
2025-05-24 22:06:06 +0300

    Add files + main basic funcs

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,60 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"github.com/CryptoCrowd/internal/config"
+	"github.com/CryptoCrowd/internal/db"
+	"github.com/CryptoCrowd/internal/logger"
+	"github.com/CryptoCrowd/migrate"
+)
 
+const (
+	configPath = "config.json"
+)
+
+func main() {
+	ctx, mainCtxCancel := context.WithCancel(context.Background())
+	defer mainCtxCancel()
+
+	cfg, err := config.Load(configPath)
+	if err != nil {
+		logger.Fatalf("ошибка загрузки конфигурации: %v", err)
+	}
+
+	err = logger.InitGlobalLogger(cfg)
+	if err != nil {
+		logger.Fatalf("ошибка инициализации логгера: %v", err)
+	}
+
+	logger.Info("Приложение запускается...")
+	logger.Infof("Используется конфигурация из файла: %s", configPath)
+
+	pool, err := initInfrastructure(ctx, cfg)
+	if err != nil {
+		logger.Fatalf("ошибка инициализации инфраструктуры: %v", err)
+	}
+	defer pool.Close()
+}
+
+// Инициализация инфраструктуры (миграции, подключение к БД)
+func initInfrastructure(ctx context.Context, cfg *config.Config) (*db.Pool, error) {
+	dbURL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		cfg.Database.User, cfg.Database.Password, cfg.Database.Host,
+		cfg.Database.Port, cfg.Database.Name)
+
+	logger.Infof("Запуск миграций для БД: %s:%s/%s", cfg.Database.Host, cfg.Database.Port, cfg.Database.Name)
+	if err := migrate.Run(dbURL, "up"); err != nil {
+		return nil, fmt.Errorf("ошибка при выполнении миграций: %v", err)
+	}
+	logger.Debug("Миграции успешно выполнены")
+
+	logger.Debug("Подключение к базе данных...")
+	pool, err := db.NewPool(ctx, dbURL)
+	if err != nil {
+		return nil, fmt.Errorf("ошибка подключения к базе данных: %v", err)
+	}
+	logger.Info("Подключение к базе данных установлено")
+
+	return pool, nil
 }
--- go.mod
+++ go.mod
@@ -3,16 +3,28 @@ module github.com/CryptoCrowd
 go 1.24.3
 
 require (
+	github.com/andybalholm/brotli v1.1.1 // indirect
+	github.com/gofiber/fiber/v2 v2.52.8 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.7.5 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/mfridman/interpolate v0.0.2 // indirect
 	github.com/pressly/goose/v3 v3.24.3 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/sethvargo/go-retry v0.3.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/crypto v0.38.0 // indirect
 	golang.org/x/sync v0.14.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
 )
