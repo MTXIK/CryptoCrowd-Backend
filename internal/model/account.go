460f70980f20a780d419cc011d59d8155d8ec9c3
neruser739 <melisakon@mail.ru>
2025-05-25 16:49:34 +0300

    Add account create check

--- internal/model/account.go
+++ internal/model/account.go
@@ -3,11 +3,11 @@ package model
 import "time"
 
 type Account struct {
-	ID           int64     `json:"id" db:"id"`
-	Username     string    `json:"username" db:"username"`
-	Email        string    `json:"email" db:"email"`
-	PasswordHash string    `json:"-" db:"password_hash"`
-	Role         string    `json:"role" db:"role"`
-	CreatedAt    time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt    time.Time `json:"updated_at" db:"updated_at"`
+	ID           int64      `json:"id,omitempty" db:"id"`
+	Username     string     `json:"username" db:"username"`
+	Email        string     `json:"email" db:"email"`
+	PasswordHash string     `json:"-" db:"password_hash"`
+	Role         string     `json:"role" db:"role"`
+	CreatedAt    *time.Time `json:"created_at,omitempty" db:"created_at"`
+	UpdatedAt    *time.Time `json:"updated_at,omitempty" db:"updated_at"`
 }
