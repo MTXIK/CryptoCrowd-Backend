eaa451cb55c7c8e4a4ddde24c3af7670dec97a5b
neruser739 <melisakon@mail.ru>
2025-05-25 17:25:02 +0300

    Add check for func data

--- internal/service/account.go
+++ internal/service/account.go
@@ -14,6 +14,7 @@ var (
 	ErrInvalidUsername = errors.New("invalid username")
 	ErrInvalidEmail    = errors.New(("invalid email"))
 	ErrInvalidRole     = errors.New("invalid role")
+	ErrEmptyPass       = errors.New("password cannot be empty")
 )
 
 type AccountRepository interface {
@@ -56,4 +57,59 @@ func (a *Account) Create(ctx context.Context, acc model.Account, plainPassword s
 	}
 
 	return a.repo.Create(ctx, acc, plainPassword)
+}
+
+func (a *Account) Update(ctx context.Context, acc model.Account) error {
+	if acc.Username == "" {
+		logger.Error("Invalid username")
+		return fmt.Errorf("%w", ErrInvalidUsername)
+	}
+
+	if !a.emailRegexp.MatchString(acc.Email) {
+		logger.Errorf("Invalid email: %s", acc.Email)
+		return fmt.Errorf("%w: %s", ErrInvalidEmail, acc.Email)
+	}
+
+	if acc.Role == "" {
+		logger.Error("Invalid role")
+		return fmt.Errorf("%w", ErrInvalidRole)
+	}
+
+	return a.repo.Update(ctx, acc)
+}
+
+func (a *Account) UpdatePassword(ctx context.Context, email string, newPassword string) error {
+	if !a.emailRegexp.MatchString(email) {
+		logger.Errorf("Invalid email: %s", email)
+		return fmt.Errorf("%w: %s", ErrInvalidEmail, email)
+	}
+
+	if newPassword == "" {
+		logger.Error("Empty new password")
+		return fmt.Errorf("%w", ErrEmptyPass)
+	}
+
+	return a.repo.UpdatePassword(ctx, email, newPassword)
+}
+
+func (a *Account) Delete(ctx context.Context, email string) error {
+	if !a.emailRegexp.MatchString(email) {
+		logger.Errorf("Invalid email: %s", email)
+		return fmt.Errorf("%w: %s", ErrInvalidEmail, email)
+	}
+
+	return a.repo.Delete(ctx, email)
+}
+
+func (a *Account) GetByEmail(ctx context.Context, email string) (model.Account, error) {
+	if !a.emailRegexp.MatchString(email) {
+		logger.Errorf("Invalid email: %s", email)
+		return model.Account{}, fmt.Errorf("%w: %s", ErrInvalidEmail, email)
+	}
+
+	return a.repo.GetByEmail(ctx, email)
+}
+
+func (a *Account) List(ctx context.Context, searchTerm string) ([]model.Account, error) {
+	return a.repo.List(ctx, searchTerm)
 }
